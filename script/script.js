ff3c3e4aa4577a626667bcd01c9ae431c9dcede0
dwatts <daniel.h.watts@gmail.com>
2025-12-18 17:45:14 -0700

    Info div updates

--- script/script.js
+++ script/script.js
@@ -28,11 +28,16 @@ $('.caravan-video').click(function (e) {
 
 /***Fade in Splash Screen on Load***/
 
+function showDivInner() {
+  $(".splash-inner").fadeIn(500);
+}
+
 $(document).ready(function(){
     $(".splash-container")
     .css("display", "flex")
     .hide()
     .fadeIn();
+    setTimeout(showDivInner, 2000);
 });
 
 /***Close Splash Screen***/
